6f6c35187b533a60642f77b836a698588add10ab
Admin <Admin@DESKTOP-F8V312A>
2022-08-25 11:45:33 +0530

    Initial Commit

---
+++ wipro/code/source/PurchaseOrder/work.java
@@ -0,0 +1,53 @@
+package PurchaseOrder;
+
+// -----( IS Java Code Template v1.2
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+// --- <<IS-END-IMPORTS>> ---
+
+public final class work
+
+{
+	// ---( internal utility methods )---
+
+	final static work _instance = new work();
+
+	static work _newInstance() { return new work(); }
+
+	static work _cast(Object o) { return (work)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void concatJS (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(concatJS)>> ---
+		// @sigtype java 3.5
+		// [i] field:0:required fName
+		// [o] field:0:required FullName
+		// pipeline
+		IDataCursor pipelineCursor = pipeline.getCursor();
+			String	fName = IDataUtil.getString( pipelineCursor, "fName" );
+		pipelineCursor.destroy();
+		
+		String FullName =  "Mr." + fName;
+		
+		// pipeline
+		IDataCursor pipelineCursor_1 = pipeline.getCursor();
+		IDataUtil.put( pipelineCursor_1, "FullName", FullName );
+		pipelineCursor_1.destroy();
+		
+			
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
---
+++ wipro/ns/PurchaseOrder/work/concatJS/java.frag
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+
+<Values version="2.0">
+  <value name="name">concatJS</value>
+  <value name="encodeutf8">true</value>
+  <value name="body">Ly8gcGlwZWxpbmUNCklEYXRhQ3Vyc29yIHBpcGVsaW5lQ3Vyc29yID0gcGlwZWxpbmUuZ2V0Q3Vy
+c29yKCk7DQoJU3RyaW5nCWZOYW1lID0gSURhdGFVdGlsLmdldFN0cmluZyggcGlwZWxpbmVDdXJz
+b3IsICJmTmFtZSIgKTsNCnBpcGVsaW5lQ3Vyc29yLmRlc3Ryb3koKTsNCg0KU3RyaW5nIEZ1bGxO
+YW1lID0gICJNci4iICsgZk5hbWU7DQoNCi8vIHBpcGVsaW5lDQpJRGF0YUN1cnNvciBwaXBlbGlu
+ZUN1cnNvcl8xID0gcGlwZWxpbmUuZ2V0Q3Vyc29yKCk7DQpJRGF0YVV0aWwucHV0KCBwaXBlbGlu
+ZUN1cnNvcl8xLCAiRnVsbE5hbWUiLCBGdWxsTmFtZSApOw0KcGlwZWxpbmVDdXJzb3JfMS5kZXN0
+cm95KCk7DQoNCgk=</value>
+</Values>
